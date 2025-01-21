ba2379bc946ad3615e3ba22ec5d33abe667a6b47
Bruno Rocha <brunorocha@vipaconnect.com>
2025-01-21 22:37:26 +0000

    fix routes for deployment

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 3001
+        port: 5045
     },
     base: '/admin/',
 });
--- langchain/app.py
+++ langchain/app.py
@@ -295,4 +295,4 @@ def healthcheck():
     return jsonify({'status': 'healthy'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True, host='0.0.0.0', port=5055)
