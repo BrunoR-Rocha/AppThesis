828899ea184eae7929cb2edf880090f6e47d64a3
BrunoR-Rocha <brunor_gon_rocha@hotmail.com>
2025-02-16 20:31:09 +0000

    new footer and dockerfile configs

--- frontend/src/resources/components/main/Footer.js
+++ frontend/src/resources/components/main/Footer.js
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 import Wrapper from "../general/Wrapper";
 import AuthContext from "../../../context/AuthContext";
 import { useTranslation, Trans } from "react-i18next";
+import { ReactComponent as Logo } from "../../media/navbar/logo_moony.svg";
+import FooterLogo from "../../media/footer/footer_logo.svg";
 
 const FooterWrapper = styled("div")`
   position: relative;
-  background-color: #8373e8;
+  background-color: #080a2a;
+  background-image: url(${FooterLogo});
   backdrop-filter: blur(5px);
+  border-top: 1px solid rgba(96, 120, 223, 0.4);
   box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.1);
   top: 0;
   left: 0;
-  min-height: 20vh;
   z-index: 10;
   padding: 20px 0;
   margin: 5px;
@@ -25,13 +28,13 @@ function Footer() {
   const { isAuthenticated } = useContext(AuthContext);
 
   return (
-    <FooterWrapper>
+    <FooterWrapper className="min-h-[300px] md:min-h-[600px] bg-no-repeat bg-right">
       <Wrapper>
         <div className="flex flex-col h-full w-full justify-between gap-5">
           <div className="flex flex-col md:flex-row w-full items-center justify-between text-white mt-auto">
-            <div className="flex flex-col md:flex-row items-start gap-10 w-full justify-center">
+            <div className="flex flex-col md:flex-row items-start gap-10 w-full justify-between relative">
               <div className="md:basis-2/5 flex justify-end flex-wrap flex-col gap-3 text-start">
-                <h4 className="text-2xl md:text-3xl text-white font-semibold font-sans">
+                {/* <h4 className="text-2xl md:text-3xl text-white font-semibold font-sans">
                   <Trans
                     i18nKey="footer.title"
                     components={{
@@ -44,14 +47,18 @@ function Footer() {
                       ),
                     }}
                   />
-                </h4>
+                </h4> */}
+                <Logo />
+                <p className="text-white font-normal text-base">
+                  {t("footer.title")}
+                </p>
 
                 <p className="text-white font-normal text-base">
                   {t("footer.slogan")}
                 </p>
               </div>
 
-              <div className="md:basis-2/5 flex justify-end w-9/12 md:w-full flex-wrap flex-col gap-3 text-start">
+              {/* <div className="md:basis-2/5 flex justify-end w-9/12 md:w-full flex-wrap flex-col gap-3 text-start">
                 <h4 className="text-2xl font-semibold">
                   {t("footer.explore")}
                 </h4>
@@ -74,16 +81,16 @@ function Footer() {
                     </Link>
                   </>
                 )}
-              </div>
+              </div> */}
 
-              <div className="md:basis-1/5 flex justify-end w-9/12 md:w-full flex-wrap flex-col gap-3 text-start">
-                <h4 className="text-2xl font-semibold">Moony</h4>
+              <div className="md:basis-1/5 flex justify-end w-9/12 md:w-full flex-wrap flex-col gap-3 text-end self-end md:self-auto">
+                <h4 className="text-xl font-semibold">Links</h4>
 
-                <Link to="/privacy-policy" className="flex justify-start">
+                <Link to="/privacy-policy" className="flex justify-end">
                   {t("footer.privacy")}
                 </Link>
 
-                <Link to="/terms-conditions" className="flex justify-start">
+                <Link to="/terms-conditions" className="flex justify-end">
                   {t("footer.terms")}
                 </Link>
 
@@ -91,14 +98,22 @@ function Footer() {
                   href="https://www.livroreclamacoes.pt/Inicio/"
                   target={"_blank"}
                   rel={"noopener noreferrer"}
-                  className="flex justify-start"
+                  className="flex justify-end"
                 >
                   {t("footer.suggestions")}
+                </a>
+                <a
+                  href="#"
+                  target={"_blank"}
+                  rel={"noopener noreferrer"}
+                  className="flex justify-end"
+                >
+                  {t("footer.reports")}
                 </a>
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap md:flex-nowrap justify-center">
+          <div className="flex flex-wrap md:flex-nowrap justify-start">
             <p className="text-white text-center text-sm">
               Copyright © 2024 Moony Project
             </p>
